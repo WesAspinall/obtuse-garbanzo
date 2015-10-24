ebafc9beaeb168bcb466001441f42b27fb231301
Wesley Aspinall <wra427@gmail.com>
2015-10-24 18:10:41 -0400

    initial commit; finished a good bit of the html and css, just beginning click events

---
+++ js/main.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+import _ from 'underscore';
+import moment from 'moment';
+
+console.log('Hello, World');
+
+import player1 from './player1'
+
+
+
+// Player1 Instance
+
+// CPU Instance
+
+
+//Setting up on events
+
+button1.on('click', function(){
+
+console.log('catfish');
+
+});
---
+++ js/player1.js
@@ -0,0 +1,14 @@
+import $ from 'jquery';
+import _ from 'underscore';
+import moment from 'moment';
+
+
+let player1 = function () {
+
+
+  };
+
+};
+
+
+export default player1;
